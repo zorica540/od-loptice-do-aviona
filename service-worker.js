4a91c93dd2297f9dc8a259ab8b601f573619fdda
zorica540 <zoricakobilarev@gmail.com>
2025-06-23 12:40:53 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
